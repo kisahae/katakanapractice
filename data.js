caefc6daf6394859673abd93c80d1c08954fe341
Haenuki Sachi <sachihaenuki1@gmail.com>
2023-03-18 20:36:36 +0700

    FIrst Upload

---
+++ data.js
@@ -0,0 +1,274 @@
+export let kanji = [
+  {
+    hint: "Father/Chichi",
+    answer: "父"
+  },
+  {
+    hint: "Mother/Haha",
+    answer: "母"
+  },
+  {
+    hint: "Pluraliing suffix/Tachi",
+    answer: "達"
+  },
+  {
+    hint: "I, me/Watashi",
+    answer: "私"
+  },
+  {
+    hint: "Daughter/Girl/Musume",
+    answer: "娘"
+  },
+  {
+    hint: "Younger Brother/Otouto",
+    answer: "弟"
+  },
+  {
+    hint: "Son/Musuko",
+    answer: "息子"
+  },
+  {
+    hint: "Younger Sister/Imouto",
+    answer: "妹"
+  },
+  {
+    hint: "Older Brother/Nii/Ani",
+    answer: "兄"
+  },
+  {
+    hint: "Older Sister/Nee/Ane",
+    answer: "姉"
+  },
+  {
+    hint: "School/Gakkou",
+    answer: "学校"
+  },
+  {
+    hint: "Student/Gakusei",
+    answer: "学生"
+  },
+  {
+    hint: "Classroom/Kyoushitsu",
+    answer: "教室"
+  },
+  {
+    hint: "Canteen/Shokudou",
+    answer: "食堂"
+  },
+  {
+    hint: "Room/Heya",
+    answer: "部屋"
+  },
+  {
+    hint: "Jepang/Japan",
+    answer: "日本"
+  },
+  {
+    hint: "Buku/Hon",
+    answer: "本"
+  },
+  {
+    hint: "Dictionary/Jishou",
+    answer: "辞書"
+  },
+  {
+    hint: "Clock/Tokei",
+    answer: "時計"
+  },
+  {
+    hint: "Car/Jidousha",
+    answer: "自動車"
+  },
+  {
+    hint: "Newspaper/Shinbun",
+    answer: "新聞"
+  },
+  {
+    hint: "Vegetable/Yasai",
+    answer: "野菜"
+  },
+  {
+    hint: "Sake/Sake",
+    answer: "酒"
+  },
+  {
+    hint: "Milk/Gyuunyuu",
+    answer: "牛乳"
+  },
+  {
+    hint: "Fish/Sakana",
+    answer: "魚"
+  },
+  {
+    hint: "Beef/Gyuniku",
+    answer: "牛肉"
+  },
+  {
+    hint: "Bird/Tori",
+    answer: "鳥"
+  },
+  {
+    hint: "Chicken/Niwatori",
+    answer: "鶏"
+  },
+  {
+    hint: "Sheep/hitsuji",
+    answer: "羊"
+  },
+  {
+    hint: "You/Anata",
+    answer: "貴方"
+  },
+  {
+    hint: "She/Kanojo",
+    answer: "彼女"
+  },
+  {
+    hint: "He/Kare",
+    answer: "彼"
+  },
+  {
+    hint: "Dog/Inu",
+    answer: "犬"
+  },
+  {
+    hint: "Cat/Neko",
+    answer: "猫"
+  },
+  {
+    hint: "Tea/Cha",
+    answer: "茶"
+  },
+  {
+    hint: "Red/Aka",
+    answer: "赤"
+  },
+  {
+    hint: "Blue/Ao",
+    answer: "青"
+  },
+  {
+    hint: "Green/Midori",
+    answer: "緑"
+  },
+  {
+    hint: "Purple/Murasaki",
+    answer: "紫"
+  },
+  {
+    hint: "Black/Kuro",
+    answer: "黒"
+  },
+  {
+    hint: "White/Shiro",
+    answer: "白"
+  },
+  {
+    hint: "Person/Jin",
+    answer: "人"
+  },
+  {
+    hint: "Gray/Haiiro",
+    answer: "灰色"
+  },
+  {
+    hint: "Toilet/Otearai",
+    answer: "お手洗い"
+  },
+  {
+    hint: "Weather/Tenki",
+    answer: "天気"
+  },
+  {
+    hint: "Who/Dare",
+    answer: "誰"
+  },
+  {
+    hint: "I/Boku",
+    answer: "僕"
+  },
+  {
+    hint: "Employee (specific office or business)/Kaishain",
+    answer: "会社員"
+  },
+  {
+    hint: "Employee (General)/Jugyoin",
+    answer: "従業員"
+  },
+  {
+    hint: "Teacher (Specific in University/school)/Kyoshi",
+    answer: "教師"
+  },
+  {
+    hint: "Teacher (General)/Sensei",
+    answer: "先生"
+  },
+  {
+    hint: "AM/Gozen",
+    answer: "午前"
+  },
+  {
+    hint: "Home/Ie",
+    answer: "家"
+  },
+  {
+    hint: "PM/Gogo",
+    answer: "午後"
+  },
+  {
+    hint: "Stairs/Kaidan",
+    answer: "階段"
+  },
+  {
+    hint: "Everyday/Mainichi",
+    answer: "毎日"
+  },
+  {
+    hint: "Time/Jikan",
+    answer: "時間"
+  },
+  {
+    hint: "1/Ichi",
+    answer: "一"
+  },
+  {
+    hint: "Now/Ima",
+    answer: "今"
+  },
+  {
+    hint: "2/Ni",
+    answer: "二"
+  },
+  {
+    hint: "3/San",
+    answer: "三"
+  },
+  {
+    hint: "4/Yon",
+    answer: "四"
+  },
+  {
+    hint: "5/Go",
+    answer: "五"
+  },
+  {
+    hint: "6/Roku",
+    answer: "六"
+  },
+  {
+    hint: "7/Nana",
+    answer: "七"
+  },
+  {
+    hint: "8/Hachi",
+    answer: "八"
+  },
+  {
+    hint: "9/Kyu",
+    answer: "九"
+  },
+  {
+    hint: "10/Juu",
+    answer: "十"
+  }
+]
---
+++ index.js
@@ -0,0 +1,53 @@
+import { kanji } from "./data.js"
+const form = document.querySelector("#form")
+const hint = document.querySelector("#hint")
+const userAnswer = document.querySelector("input")
+const description = document.querySelector("#description")
+const checkButton = document.querySelector("#checkButton")
+const correctAnswer = document.querySelector("#correctAnswer")
+let trueAnswer = 0
+let remainingQuestion = document.querySelector("#remainingQuestion")
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+})
+
+let no = Math.floor(Math.random() * kanji.length)
+remainingQuestion.textContent = kanji.length
+correctAnswer.textContent = trueAnswer
+hint.textContent = kanji[no].hint
+
+const pointCount = (no, des, point) => {
+  description.textContent = des
+  trueAnswer = trueAnswer + point
+  correctAnswer.textContent = trueAnswer
+  kanji.splice(no, 1)
+}
+
+checkButton.addEventListener("click", () => {
+  if (userAnswer.value === kanji[no].answer) {
+    Swal.fire({
+      title: "Correct!",
+      text: "You are correct",
+      icon: "success",
+      confirmButtonText: "Keep Going"
+    })
+    pointCount(no, "Correct", 1)
+  } else {
+    Swal.fire({
+      title: "Incorrect!",
+      text: `The correct answer is ${kanji[no].answer}`,
+      icon: "error",
+      confirmButtonText: "Ok, I will remember it"
+    })
+    pointCount(no, "Incorrect", 0)
+  }
+  no = Math.floor(Math.random() * kanji.length)
+  if (kanji.length) {
+    remainingQuestion.textContent = kanji.length
+    hint.textContent = kanji[no].hint
+  } else {
+    remainingQuestion.textContent = kanji.length
+    hint.textContent = "No Question"
+  }
+})
